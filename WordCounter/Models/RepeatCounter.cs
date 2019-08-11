25a8332373726be29b31b8aea7503afae7f636a9
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-08-11 15:25:49 -0700

    Made more comments

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -8,32 +8,12 @@ namespace WordCounter.Models
     private string Word { get; set; }
     private string Sentence { get; set; }
 
+    // First, lower case user input
     // Create new instance of RepeatCounter - Converts case to lower and adds space on both sides in case the word appears are the very beginning or end of the sentance.
     public RepeatCounter(string userWord, string userSentance)
     {
       Word = userWord.ToLower();
       Sentence = $" {userSentance.ToLower()} ";
-    }
-
-    // Method analizes character in front of and behind the word and invalidates the instance of word if it is a letter or digit.
-    public bool IsCharacterDividing(char character)
-    {
-      return !Char.IsLetterOrDigit(character);
-    }
-
-    // Method takes beginning and ending character and sends it to IsCharacterDividing.
-    public bool AreCharactersDividingWord(char previousCharacter, char followingCharacter)
-    {
-      return IsCharacterDividing(previousCharacter) && IsCharacterDividing(followingCharacter);
-    }
-
-    // Method takes padded word and isolates the beginning/previousCharacter and end/followingCharacter before sending it to AreCharactersDividingWord
-    public bool IsValidWordInSentence(string paddedWord)
-    {
-      char previousCharacter = paddedWord[0];
-      char followingCharacter = paddedWord[paddedWord.Length - 1];
-
-      return AreCharactersDividingWord(previousCharacter, followingCharacter);
     }
 
     // Method takes in user word and sentence and created a padded version of word before sendng it to IsValidWordInSentence
@@ -43,13 +23,14 @@ namespace WordCounter.Models
 
       int count = 0;
       int position = 0;
+      
       // Padded word creation to get the character in front of and behind an instance of word.
       while (position < Sentence.Length - 1)
       {
         int startIndex = Sentence.IndexOf(Word, position) - 1;
         int endIndex = startIndex + wordLength;
 
-        // Break to avoid an infinate loop.
+        // Break to be sure startIndex isn't negative
         if (startIndex < 0)
         {
           break;
@@ -68,5 +49,29 @@ namespace WordCounter.Models
 
       return count;
     }
+
+    // Method takes padded word and isolates the beginning/previousCharacter and end/followingCharacter before sending it to AreCharactersDividingWord
+    public bool IsValidWordInSentence(string paddedWord)
+    {
+      char previousCharacter = paddedWord[0];
+      char followingCharacter = paddedWord[paddedWord.Length - 1];
+
+      return AreCharactersDividingWord(previousCharacter, followingCharacter);
+    }
+
+    // Method takes beginning and ending character and sends it to IsCharacterDividing.
+    public bool AreCharactersDividingWord(char previousCharacter, char followingCharacter)
+    {
+      return IsCharacterDividing(previousCharacter) && IsCharacterDividing(followingCharacter);
+    }
+
+    // Method analizes character in front of and behind the word and invalidates the instance of word if it is a letter or digit.
+    public bool IsCharacterDividing(char character)
+    {
+      return !Char.IsLetterOrDigit(character);
+    }
+
+
+
   }
 }
